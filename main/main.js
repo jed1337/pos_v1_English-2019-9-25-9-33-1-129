74e718a8330f4139ef952aa08c34356ce478acf8
jed1337 <j.caychingco@yahoo.com>
2019-10-19 13:51:07 +0800

    Add print receipt method

--- main/main.js
+++ main/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-function decodeBarcodes(barcodes){
-    const decodedBarcodes=[];
+function decodeBarcodes(barcodes) {
+    const decodedBarcodes = [];
 
-    for(let rawBarcode of barcodes){
+    for (let rawBarcode of barcodes) {
         const actualBarcode = rawBarcode.split('-')[0];
         let count = Number(rawBarcode.split('-')[1]);
 
-        if(isNaN(count)){
+        if (isNaN(count)) {
             count = Number(1);
         }
 
@@ -20,34 +20,34 @@ function decodeBarcodes(barcodes){
     return decodedBarcodes;
 }
 
-function combineBarcodes(decodedBarcodes){
+function combineBarcodes(decodedBarcodes) {
     const distinctBarcodes = Array.from(new Set(
-        decodedBarcodes.map(db=>db.barcode)
+        decodedBarcodes.map(db => db.barcode)
     ));
     const combinedBarcodes = [];
 
-    for(let distinctBarcode of distinctBarcodes){
+    for (let distinctBarcode of distinctBarcodes) {
         const totalCount = decodedBarcodes
-            .filter(db=>db.barcode===distinctBarcode)
-            .map(db=>db.count)
-            .reduce((acc, b)=>acc+b, 0);
+            .filter(db => db.barcode === distinctBarcode)
+            .map(db => db.count)
+            .reduce((acc, b) => acc + b, 0);
 
         combinedBarcodes.push(
-            {barcode: distinctBarcode, count: totalCount}
+            { barcode: distinctBarcode, count: totalCount }
         );
     }
 
     return combinedBarcodes;
 }
 
-function getItemsWithCount(combinedBarcodes){
+function getItemsWithCount(combinedBarcodes) {
     let allItems = loadAllItems();
-    let itemsWithCount=[];
-    
-    for(let combinedBarcode of combinedBarcodes){
+    let itemsWithCount = [];
+
+    for (let combinedBarcode of combinedBarcodes) {
         let loadedItem = allItems
-            .filter(item=>item.barcode === combinedBarcode.barcode)
-            [0];
+            .filter(item => item.barcode === combinedBarcode.barcode)
+        [0];
 
         loadedItem.count = combinedBarcode.count;
         itemsWithCount.push(loadedItem);
@@ -56,25 +56,25 @@ function getItemsWithCount(combinedBarcodes){
     return itemsWithCount;
 }
 
-function calculateTotalPrice(item){
-    return item.price*item.count;
+function calculateTotalPrice(item) {
+    return item.price * item.count;
 }
 
-function calculateDiscountedPrice(item){
+function calculateDiscountedPrice(item) {
     const promotions = loadPromotions();
-    for(let promotion of promotions){
-        if(promotion.type==="BUY_TWO_GET_ONE_FREE"){
-            if(promotion.barcodes.includes(item.barcode)){
-                return calculateTotalPrice(item)-Math.floor(item.count/3)*item.price;
+    for (let promotion of promotions) {
+        if (promotion.type === "BUY_TWO_GET_ONE_FREE") {
+            if (promotion.barcodes.includes(item.barcode)) {
+                return calculateTotalPrice(item) - Math.floor(item.count / 3) * item.price;
             }
         }
     }
     return calculateTotalPrice(item);
 }
 
-function getReceiptItems(items){
-    let receiptItems=[];
-    for(let item of items){
+function getReceiptItems(items) {
+    let receiptItems = [];
+    for (let item of items) {
         let receiptItem = item;
         receiptItem.normalPrice = calculateTotalPrice(item);
         receiptItem.discountedPrice = calculateDiscountedPrice(item);
@@ -84,27 +84,47 @@ function getReceiptItems(items){
     return receiptItems;
 }
 
-function printSingleReceiptItem(item){
-    return `Name: ${item.name}, `+
-        `Quantity: ${item.count} ${item.unit}s, `+
-        `Unit: ${item.price.toFixed(2)}(yuan), `+
+function printSingleReceiptItem(item) {
+    return `Name: ${item.name}, ` +
+        `Quantity: ${item.count} ${item.unit}s, ` +
+        `Unit: ${item.price.toFixed(2)}(yuan), ` +
         `Subtotal: ${item.discountedPrice.toFixed(2)}(yuan)`;
 }
 
 
-function printReceipt(items){
-    // let receipt = '***<store earning no money>Receipt ***\n';
+function printReceipt(items) {
+    let receiptItems = getReceiptItems(getItemsWithCount(combineBarcodes(decodeBarcodes(items))));
 
-    // for(let item of items){
-    //     receipt+=`Name ${item.name}, Quantity: $`
-    // }
+    let receipt = '***<store earning no money>Receipt ***\n';
+    for (let receiptItem of receiptItems) {
+        receipt += `${printSingleReceiptItem(receiptItem)}\n`;
+    }
 
-    return `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************`;
+    receipt += `----------------------\n`;
+
+    const total = receiptItems
+        .map(receiptItem => receiptItem.discountedPrice)
+        .reduce((a, b) => a + b, 0)
+        .toFixed(2);
+
+    const discountedPrices = receiptItems
+        .map(receiptItem => receiptItem.normalPrice-receiptItem.discountedPrice)
+        .reduce((a, b) => a + b, 0)
+        .toFixed(2);
+
+    receipt += `Total: ${total}(yuan)\n`
+    receipt += `Discounted prices: ${discountedPrices}(yuan)\n`
+    
+    receipt += `**********************`;
+
+    return receipt;
+
+//     return `***<store earning no money>Receipt ***
+// Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
+// Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
+// Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
+// ----------------------
+// Total：58.50(yuan)
+// Discounted prices：7.50(yuan)
+// **********************`;
 }
--- test/main-test.js
+++ test/main-test.js
@@ -206,12 +206,12 @@ describe('pos', () => {
     const actual = printReceipt(tags);
 
     const expectText = `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
+Name: Sprite, Quantity: 5 bottles, Unit: 3.00(yuan), Subtotal: 12.00(yuan)
+Name: Litchi, Quantity: 2.5 pounds, Unit: 15.00(yuan), Subtotal: 37.50(yuan)
+Name: Instant Noodles, Quantity: 3 bags, Unit: 4.50(yuan), Subtotal: 9.00(yuan)
 ----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
+Total: 58.50(yuan)
+Discounted prices: 7.50(yuan)
 **********************`;
 
     expect(actual).toBe(expectText);
