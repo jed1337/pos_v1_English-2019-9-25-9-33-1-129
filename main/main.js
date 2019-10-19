aabd209fe06a496a9c76de54ef56fd6b3d8fcc07
jed1337 <j.caychingco@yahoo.com>
2019-10-19 13:59:00 +0800

    Removed comments

--- main/main.js
+++ main/main.js
@@ -118,13 +118,4 @@ function printReceipt(items) {
     receipt += `**********************`;
 
     return receipt;
-
-//     return `***<store earning no money>Receipt ***
-// Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-// Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-// Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-// ----------------------
-// Total：58.50(yuan)
-// Discounted prices：7.50(yuan)
-// **********************`;
 }
